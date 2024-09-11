8cb6376f3170658a8adc796c9e82e6ffbc63604d
AbdulkalamAasath <142470162+AbdulkalamAasath@users.noreply.github.com>
2024-09-11 20:22:24 +0530

    Enquiry update

--- Backend/Modules/Supplier.js
+++ Backend/Modules/Supplier.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema
 const EntrySchema = new Schema(
 {
    SupplierEnquiry:{type:String},
-   StuSupplier:{type:String} 
+   StuSupplier:{type:String},
+    
 }
 )  
 module.exports = mongoose.model('Supplier-Entry',EntrySchema)
